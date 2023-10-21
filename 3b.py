29706d761668846b3b8720fd29003b2118afc53e
basra4ka <evgeha_ivanov_01@bk.ru>
2023-10-21 17:05:14 +0300

    commit

---
+++ 3b.py
@@ -0,0 +1,41 @@
+#Красно-черное дерево
+
+class Node():
+    def __init__(self, item):
+        self.item = item
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.color = 1 # 0 - черное, 1 - красное
+
+colors = ["черное", "красное"]
+
+class Tree():
+    def __init__(self):
+        self.nullNode = Node(0)
+        self.nullNode.color = 0
+        self.nullNode.left = None
+        self.nullNode.right = None
+        self.root = self.nullNode
+
+    def printTree():
+        printNode(self.root, False)
+
+    space = ""
+
+    def printNode(node, isLeft):
+        if node != nullNode:
+            print(space)
+            if isLeft:
+                print("L-----")
+                space += "|    "
+            else:
+                print("R-----")
+                space += "     "
+
+
+        print(node.item, "(", colors[node.color], ")")
+        printNode(node.left, True)
+        printNode(node.right, False)            
+
+    
